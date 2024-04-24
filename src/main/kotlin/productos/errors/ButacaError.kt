3e96fce9c2d991c2b016d99ed49f2f2654bb2295
yahya el hadri <yahyaelhadricgs@gmail.com>
2024-04-24 13:03:51 +0200

    commit del service completo de butacas

--- src/main/kotlin/productos/errors/ButacaError.kt
+++ src/main/kotlin/productos/errors/ButacaError.kt
@@ -4,4 +4,10 @@ package org.example.productos.errors.butaca
 
 sealed class ButacaError(val mensage:String) {
     class IdNoValido(mensage: String):ButacaError(mensage)
+    class TipoInvalido(mensage: String):ButacaError(mensage)
+    class FechaInvalido(mensage: String):ButacaError(mensage)
+    class FicheroNoValido(mensage: String):ButacaError(mensage)
+    class ButacaNoEncontrada(mensage: String):ButacaError(mensage)
+    class ButacaNoBorradas(mensage: String):ButacaError(mensage)
+    class ButacaNoActualizadas(mensage: String):ButacaError(mensage)
 }
