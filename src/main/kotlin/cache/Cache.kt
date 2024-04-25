528239791f8728dba079850fbb23b3827a6db937
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2024-04-25 20:34:21 +0200

    Conflictos resueltos

--- src/main/kotlin/cache/Cache.kt
+++ src/main/kotlin/cache/Cache.kt
@@ -2,14 +2,9 @@ package org.example.cache
 
 import org.example.cache.errors.CacheError
 import com.github.michaelbull.result.Result
-<<<<<<< HEAD
 
 interface Cache<K, T> {
-    fun get(key: K): Result<T,CacheError>
-=======
-interface Cache<K, T> {
     fun get(key: K): Result<T, CacheError>
->>>>>>> feature/serviceCliente
     fun put(key: K, value: T): Result<T, Nothing>
     fun remove(key: K): Result<T, CacheError>
     fun clear(): Result<Unit, Nothing>
